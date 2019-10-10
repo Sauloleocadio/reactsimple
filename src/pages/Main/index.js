6e0a59cb89ddb43a7e2985d5aa08df5e435ab642
Sauloleocadio <saulomanshu@gmail.com>
2019-10-10 16:30:28 -0300

    Inicialização do projeto

---
+++ src/pages/Main/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
+
+import api from '../../services/api.js';
+
+export default class Main extends Component {
+    state = {
+        newRepo: '',
+    };
+
+    handleInputChange = e => {
+        this.setState({ newRepo: e.target.value });
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const { newRepo } = this.state;
+        const response = await api.get(`/repos/${newRepo}`);
+        console.log(response.data);
+    };
+
+    render() {
+        const { newRepo } = this.state;
+
+        return (
+            <Container>
+                <h1>
+                    <FaGithubAlt />
+                    Repositórios
+                </h1>
+
+                <Form onClick={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Adicionar repositório"
+                        value={newRepo}
+                        onChange={this.handleInputChange}
+                    />
+                    <SubmitButton>
+                        <FaPlus color="#FFF" size={14} />
+                    </SubmitButton>
+                </Form>
+            </Container>
+        );
+    }
+}
